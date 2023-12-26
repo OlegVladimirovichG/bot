3ee9fc3ae06d7e0ebbb82ef1b664536b7773b677
OlegVladimirovichG <111046430+OlegVladimirovichG@users.noreply.github.com>
2023-12-26 20:03:06 +0300

    Update config.py

--- logic/config.py
+++ logic/config.py
@@ -5,8 +5,8 @@ from aiogram import Bot
 
 @dataclass
 class Secrets:
-    BOT_TOKEN: str = "6678253357:AAGRLewX8Ck3R0g9A3iiNum7lHiFf_RNQfU"
-    CHAT_ID: int = 1222943667
+    BOT_TOKEN: str = "****"
+    CHAT_ID: int = ****
 
 
 bot = Bot(token=Secrets.BOT_TOKEN)
